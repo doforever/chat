41bf4cd4d3a38a40007bbc01df310442ba09044e
Dorota Tomala <doforever@gmail.com>
2021-04-25 14:44:49 +0200

    Serve app form any link

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const messages = [];
+
 app.use(express.static(path.join(__dirname, '/client')));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-});
-
-app.use((req, res) => {
-  res.status(404).send('Not found...');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
 app.listen(8000, () => {
